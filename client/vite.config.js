da735c7c84022e71dc25d338aacc8f6e4892d8f1
Udaya Bhanu <143161328+bhanu907@users.noreply.github.com>
2025-08-06 17:38:40 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
-    port: 3000,
+    port: 3000
   },
-  optimizeDeps: {
-    exclude: ["lucide-react"],
-  },
-});
+  // 👇 This is the most important part for Vercel
+  base: './',
+})
